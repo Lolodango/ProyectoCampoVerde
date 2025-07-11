2fb4f96a06b213039a6e6000ec887eaff24e9e19
CR-Alejandro2905 <rojaspiedraalejandro29@gmail.com>
2025-07-11 10:13:14 -0600

    Se agregaro una pág de para ver los pagos que han sido realizados por usuarios.
    Se agregó una pág para realizar el pago de un mantenimiento
    
    Se creó una carpeta js con scripts necesarios
    
    Se corrigió link de css/style.css

---
+++ js/script_quejas_admin.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const filtroBtns = document.querySelectorAll(".filtro-btn");
+    const filas = document.querySelectorAll("#tablaQuejas tbody tr");
+
+    filtroBtns.forEach(btn => {
+      btn.addEventListener("click", () => {
+        filtroBtns.forEach(b => b.classList.remove("active"));
+        btn.classList.add("active");
+
+        const filtro = btn.getAttribute("data-filtro");
+
+        filas.forEach(fila => {
+          if (filtro === "todas") {
+            fila.style.display = "";
+          } else {
+            fila.style.display = fila.getAttribute("data-estado") === filtro ? "" : "none";
+          }
+        });
+      });
+    });
+
+  
+});
